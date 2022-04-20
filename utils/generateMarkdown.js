b9a217f61d8dfe36f53b0a50b78914657d90a1a7
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-20 09:31:55 -0700

    add license part 2

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,7 +15,17 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === licenseArr[0]) {
+    return `(https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === licenseArr[1]) {
+    return `(https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (license === licenseArr [2]) {
+    return `(https://opensource.org/licenses/MIT)`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
