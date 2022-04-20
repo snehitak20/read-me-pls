0981bef9bfe3721dae3231ee7f523d13d5ba7371
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-20 00:09:04 -0700

    pseudocode complete

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Prompt: 
+    // Application repository [Name of the application's repository]
+    // Title [Title of the readme] 
+    // Table of contents --> links take you to the respective areas --> WITHIN generatemarkdown
+    // Description [Give a short description about your project] 
+    // Installation [How to install this application] 
+    // Usage [How to navigate this application] 
+    // License: give a list of options--> badge at the top of the page, explanation of license at the bottom [SEE LICENSE.js file]
+    // Contributors [Who are the contributors?] 
+    // Tests???
+    // QUESTIONS 
+        // GitHub username--> links to GitHub profile 
+        // If there any additional questions, reach me at EMAIL?
+// NEW README: call it generate.md 
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
